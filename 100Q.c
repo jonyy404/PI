b46eafa630514f2fa9f9f468050d9500d11040a5
jonyy404 <73437953+jonyy404@users.noreply.github.com>
2021-06-07 18:55:26 +0100

    Update 100Q.c

--- 100Q.c
+++ 100Q.c
@@ -42,3 +42,110 @@ LInt reverseL (LInt l){
     	l = last;
     	return l;
 }
+
+55 -
+
+void insertOrd (LInt *l, int x){
+    LInt aux = *l;
+    LInt ant = NULL;
+    LInt novo = (LInt) malloc(sizeof(struct lligada));
+    while (aux != NULL && aux->valor < x) {
+        ant = aux;
+        aux = aux->prox;
+    }
+    novo -> valor = x;
+    novo -> prox = aux;
+    if (ant == NULL){
+        (*l) = novo;
+        novo->prox = aux;
+    }
+    else {
+        ant->prox = novo;
+        novo->prox = aux;
+    }
+}
+
+56 -
+
+int removeOneOrd (LInt *l, int x){
+    int r=1;
+    LInt aux = *l;
+    LInt ant = NULL;
+    while (aux != NULL && aux->valor != x) {
+        ant = aux;
+        aux = aux->prox;
+    }
+    if (ant == NULL) {
+        if (aux != NULL) {
+            free(aux);
+            (*l) = NULL;
+            r=0;
+        }
+    }
+    else {
+        if (aux != NULL) {
+            free(aux);
+            ant->prox = aux->prox;
+            r=0;
+        }
+    }
+    return r;
+}
+
+57 - 
+
+59 -
+
+LInt parteAmeio (LInt *l){
+    LInt aux = *l;
+    LInt ant = NULL;
+    LInt r; 
+    int m = (length(*l)/2);
+    if (aux == NULL) return NULL;
+    if (m<=0) return NULL; 
+    while (m>0) {
+        if (ant == NULL) {
+            r = aux;
+        }
+        else {
+            ant->prox = aux;
+        }
+        ant = aux;
+        aux = aux->prox;
+        m--;        
+    }
+    *l = aux;
+    ant->prox = NULL;    
+    
+    return r;
+}
+
+60 -
+
+int removeAll (LInt *l, int x){
+    LInt ant;
+    int c = 0;
+    while (*l != NULL){
+        ant = *l;
+        if ((*l)->valor == x){
+            *l = (*l) -> prox;
+            ant -> prox = *l;
+            c++;
+        } else l = &((*l)->prox);
+    }
+    return c;
+}
+
+61 - 
+
+int removeDups (LInt *l){
+    LInt ant = NULL;
+    int x;
+    int c=0;
+    while (*l != NULL){
+        x = (*l)->valor;
+        c += removeAll(&((*l)->prox),x);
+        l = &((*l)->prox);
+    }
+    return c;
+}
